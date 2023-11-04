030c3090686483a3ab7fb4eb6fa2888a17690e78
chewzzz <chewziqing@gmail.com>
2023-11-04 14:13:16 +0800

    json-server; conditional render

--- vue-simple-router/src/views/jobs/JobDetail.vue
+++ vue-simple-router/src/views/jobs/JobDetail.vue
@@ -1,6 +1,12 @@
 <template>
-  <h1>Job Details</h1>
-  <p>The job id is {{ id }}</p>
+  <div v-if="isLoaded">
+    <h1>{{ job.title }}</h1>
+    <p>The job id is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading job details...</p>
+  </div>
 </template>
 
 <script>
@@ -10,8 +16,19 @@ export default {
     data() {
         return {
             // id: this.$route.params.id
+            job: {},
+            isLoaded: false,
         }
-    }
+    },
+    mounted() {
+        fetch(`http://localhost:3000/jobs/${this.id}`)
+            .then(res => res.json())
+            .then(data => {
+                this.job = data
+                this.isLoaded = true
+            })
+            .catch(err => console.log(err.message))
+    },
 }
 </script>
 
