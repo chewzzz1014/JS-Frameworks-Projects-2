ee48907ef6cc82ce02893fe05b2260ddfb7702ab
chewzzz <chewziqing@gmail.com>
2023-11-04 15:14:30 +0800

    composition api

---
+++ vue-blog/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    home
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'Home',
+  setup() {
+    const p = ref(null)
+
+    let name = 'chewzzz'
+    let age = 21
+
+    const handleClick = () => {
+      // console.log(p, p.value)
+      // p.value.classList.add('test')
+      // p.value.textContent = 'hello world'
+      
+    }
+
+    return {name, age, handleClick, p}
+  },
+}
+</script>
+
+<style>
+
+</style>
