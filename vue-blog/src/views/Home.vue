0cdecd05271077bb3061d7376f7cda36226f4af3
chewzzz <chewziqing@gmail.com>
2023-11-04 16:33:01 +0800

    composition API: ref, reactive, computed, watch, watchEffect

--- vue-blog/src/views/Home.vue
+++ vue-blog/src/views/Home.vue
@@ -1,29 +1,64 @@
 <template>
   <div class="home">
-    home
-    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">click me</button>
+    <h1>Home</h1>
+
+    <h2>Refs</h2>
+    <p>{{ personOne.name }} - {{ personOne.age }}</p>
+    <button @click="updatePersonOne">Update person one</button>
+
+    <h2>Reactive</h2>
+    <p>{{ personTwo.name }} - {{ personTwo.age }}</p>
+    <button @click="updatePersonTwo">Update person two</button>
+
+    <h2>Computed and watch/watchEffect</h2>
+    <input type="text" v-model="search">
+    <p>search term: {{ search }}</p>
+    <div v-for="(name, key) in matchingNames" :key="key">
+      {{ name }}
+    </div>
+    <button @click="handleClick">Stop Watching</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive, computed, watch, watchEffect } from 'vue'
 export default {
   name: 'Home',
   setup() {
-    const p = ref(null)
+    const personOne = ref({name: 'chewzzz', age: 21})
+    const personTwo = reactive({name: 'Moon', age: 30}) // reactive doesnt work with primitive value (can't update them)
 
-    let name = 'chewzzz'
-    let age = 21
-
-    const handleClick = () => {
-      // console.log(p, p.value)
-      // p.value.classList.add('test')
-      // p.value.textContent = 'hello world'
-      
+    const updatePersonOne = () => {
+      personOne.value.age = 30
     }
 
-    return {name, age, handleClick, p}
+    const updatePersonTwo = () => {
+      personTwo.age = 45
+    }
+
+    // for computed
+    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
+    const search = ref('')
+    const matchingNames = computed(() => {
+      return names.value.filter(x => x.includes(search.value))
+    })
+
+    // watch and watchEffect
+    const stopWatch = watch(search, () => {
+      console.log('watch function ran')
+    })
+    const stopEffect = watchEffect(() => {
+      // watch state - search also, but does not need to mention it explicitly
+      // will automatically watch state used inside this
+      // if no state is insides, then only run once at the beginning
+      console.log('watchEffect function ran', search.value)
+    })
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
+    }
+
+    return {personOne, personTwo, updatePersonOne, updatePersonTwo, names, search, matchingNames, handleClick}
   },
 }
 </script>
