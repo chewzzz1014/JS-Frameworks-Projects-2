bcd4cc096537462f0f1158e955a2687bc80358b6
chewzzz <chewziqing@gmail.com>
2023-11-06 20:30:16 +0800

    composition: props and lifecycle hook in setup

---
+++ vue-blog/src/views/CompositionExample.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="home">
+    <h1>Composition Tutorial and Examples</h1>
+
+    <h2>Refs</h2>
+    <p>{{ personOne.name }} - {{ personOne.age }}</p>
+    <button @click="updatePersonOne">Update person one</button>
+
+    <h2>Reactive</h2>
+    <p>{{ personTwo.name }} - {{ personTwo.age }}</p>
+    <button @click="updatePersonTwo">Update person two</button>
+
+    <h2>Computed and watch/watchEffect</h2>
+    <input type="text" v-model="search">
+    <p>search term: {{ search }}</p>
+    <div v-for="(name, key) in matchingNames" :key="key">
+      {{ name }}
+    </div>
+    <button @click="handleClick">Stop Watching</button>
+  </div>
+</template>
+
+<script>
+import { ref, reactive, computed, watch, watchEffect } from 'vue'
+export default {
+  name: 'CompositionExample',
+  setup() {
+    const personOne = ref({name: 'chewzzz', age: 21})
+    const personTwo = reactive({name: 'Moon', age: 30}) // reactive doesnt work with primitive value (can't update them)
+
+    const updatePersonOne = () => {
+      personOne.value.age = 30
+    }
+
+    const updatePersonTwo = () => {
+      personTwo.age = 45
+    }
+
+    // for computed
+    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
+    const search = ref('')
+    const matchingNames = computed(() => {
+      return names.value.filter(x => x.includes(search.value))
+    })
+
+    // watch and watchEffect
+    const stopWatch = watch(search, () => {
+      console.log('watch function ran')
+    })
+    const stopEffect = watchEffect(() => {
+      // watch state - search also, but does not need to mention it explicitly
+      // will automatically watch state used inside this
+      // if no state is insides, then only run once at the beginning
+      console.log('watchEffect function ran', search.value)
+    })
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
+    }
+
+    return {personOne, personTwo, updatePersonOne, updatePersonTwo, names, search, matchingNames, handleClick}
+  },
+}
+</script>
+
+<style>
+
+</style>
--- vue-blog/src/views/Home.vue
+++ vue-blog/src/views/Home.vue
@@ -1,64 +1,26 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-
-    <h2>Refs</h2>
-    <p>{{ personOne.name }} - {{ personOne.age }}</p>
-    <button @click="updatePersonOne">Update person one</button>
-
-    <h2>Reactive</h2>
-    <p>{{ personTwo.name }} - {{ personTwo.age }}</p>
-    <button @click="updatePersonTwo">Update person two</button>
-
-    <h2>Computed and watch/watchEffect</h2>
-    <input type="text" v-model="search">
-    <p>search term: {{ search }}</p>
-    <div v-for="(name, key) in matchingNames" :key="key">
-      {{ name }}
-    </div>
-    <button @click="handleClick">Stop Watching</button>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
-import { ref, reactive, computed, watch, watchEffect } from 'vue'
+import {ref} from 'vue'
+import PostList from '../components/PostList.vue'
+
 export default {
   name: 'Home',
+  components: {
+    PostList,
+  },
   setup() {
-    const personOne = ref({name: 'chewzzz', age: 21})
-    const personTwo = reactive({name: 'Moon', age: 30}) // reactive doesnt work with primitive value (can't update them)
+    const posts = ref([
+      {title: 'Welcome to the blog', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita similique in mollitia, nihil qui ad iusto ratione pariatur. Sit placeat cupiditate ipsam nobis adipisci. Quam itaque sed eos! Ab, ipsa! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam accusamus, sint fugiat eveniet aliquid ex, assumenda maxime, pariatur itaque dolore quis natus facilis consequatur nemo beatae? Laudantium consequuntur iusto repellat.', id: 1},
+      {title: 'Top 5 CSS tips', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita similique in mollitia, nihil qui ad iusto ratione pariatur. Sit placeat cupiditate ipsam nobis adipisci. Quam itaque sed eos! Ab, ipsa! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam accusamus, sint fugiat eveniet aliquid ex, assumenda maxime, pariatur itaque dolore quis natus facilis consequatur nemo beatae? Laudantium consequuntur iusto repellat.', id: 2},
+    ])
 
-    const updatePersonOne = () => {
-      personOne.value.age = 30
-    }
-
-    const updatePersonTwo = () => {
-      personTwo.age = 45
-    }
-
-    // for computed
-    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
-    const search = ref('')
-    const matchingNames = computed(() => {
-      return names.value.filter(x => x.includes(search.value))
-    })
-
-    // watch and watchEffect
-    const stopWatch = watch(search, () => {
-      console.log('watch function ran')
-    })
-    const stopEffect = watchEffect(() => {
-      // watch state - search also, but does not need to mention it explicitly
-      // will automatically watch state used inside this
-      // if no state is insides, then only run once at the beginning
-      console.log('watchEffect function ran', search.value)
-    })
-    const handleClick = () => {
-      stopWatch()
-      stopEffect()
-    }
-
-    return {personOne, personTwo, updatePersonOne, updatePersonTwo, names, search, matchingNames, handleClick}
+    return {posts}
   },
 }
 </script>
