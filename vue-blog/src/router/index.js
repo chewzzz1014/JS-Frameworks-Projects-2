24c73a7dd488f63991dd7f4121b5f68e2ce5ad5d
chewzzz <chewziqing@gmail.com>
2023-11-17 13:03:47 +0800

    get a single post

--- vue-blog/src/router/index.js
+++ vue-blog/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import CompositionExample from '../views/CompositionExample.vue'
+import Details from '../views/Details.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,12 @@ const router = createRouter({
       path: '/composition-example',
       name: 'CompositionExample',
       component: CompositionExample
+    },
+    {
+      path: '/posts/:id',
+      name: 'Details',
+      component: Details,
+      props: true
     }
   ]
 })
