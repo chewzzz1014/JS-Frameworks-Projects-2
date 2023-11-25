e08ea23cab0d56ee3b8c483effda79ec48b3ce4b
chewzzz <chewziqing@gmail.com>
2023-11-25 16:23:37 +0800

    move app into composition pi

--- vue-start-spa/src/App.vue
+++ vue-start-spa/src/App.vue
@@ -1,10 +1,52 @@
 <template>
-  <h1>hello world</h1>
+  <div>
+    <Navbar
+      :pages="pages"
+      :activePage="activePage"
+      @navLinkChange="updateActivePage"
+    ></Navbar>
+    <PageViewer
+      :page="pages[activePage]"
+    ></PageViewer>
+  </div>
 </template>
 
 <script>
-export default {
+import Navbar from './components/Navbar.vue'
+import PageViewer from './components/PageViewer.vue'
 
+export default {
+    components: {
+      Navbar,
+      PageViewer
+    },
+    data() {
+      return {
+        activePage: 0,
+        pages: [
+          {
+            link: {text: 'Home', url: 'home.html'},
+            pageTitle: 'Home Page',
+            content: 'This is the home content'
+          },
+          {
+            link: {text: 'About', url: 'about.html'},
+            pageTitle: 'About Page',
+            content: 'This is the about content'
+          },
+          {
+            link: {text: 'Contact', url: 'contact.html'},
+            pageTitle: 'Contact Page',
+            content: 'This is the contact content'
+          }, 
+        ],
+      }
+    },
+    methods: {
+      updateActivePage(index) {
+        this.activePage = index
+      },
+    },
 }
 </script>
 
---
+++ vue-start-spa/src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+    <nav
+        :class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar navbar-expand-lg']"
+    >
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand">My Vue</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li v-for="(page, key) in pages" class="nav-items" :key="key">
+                    <a 
+                        :href="page.link.url"
+                        class="nav-link"
+                        :class="{active: activePage === key}"
+                        aria-current="page"
+                        :title="`This link goes to ${page.link.text} page`"
+                        @click.prevent="$emit('navLinkChange', key)"
+                    >{{ page.link.text }}</a>
+            </li>
+        </ul>
+            <form class="d-flex">
+                <button
+                    class="btn btn-primary"
+                    @click.prevent="toggleTheme"
+                >
+                    Toggle
+            </button>
+            </form>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    props: ['pages', 'activePage'],
+    data() {
+        return {
+            theme: 'dark',
+        }
+    },
+    methods: {
+        toggleTheme() {
+            this.theme = this.theme === 'light' ? 'dark' : 'light'
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
