8b1182771c6e0a98a45f404b7ae613e9defd8390
chewzzz <chewziqing@gmail.com>
2023-12-06 14:28:12 +0800

    convert option api to composition api

--- vue-start-spa/src/views/CreatePage.vue
+++ vue-start-spa/src/views/CreatePage.vue
@@ -35,7 +35,7 @@
                         v-model="linkText"
                     >
                 </div>
-                <div class="mb-3">
+                <!-- <div class="mb-3">
                     <label for="" class="form-label">
                         Link URL
                     </label>
@@ -44,7 +44,7 @@
                         class="form-control"
                         v-model="linkUrl"
                     >
-                </div>
+                </div> -->
                 <div class="row mb-3">
                     <div class="form-check">
                         <input 
@@ -74,68 +74,49 @@
     </form>
 </template>
 
-<script>
-export default {
-    name: 'CreatePage',
-    emits: {
-        pageCreated({pageTitle, content, link, published}) {
-            if (!pageTitle || !content || !link || !link.text || !link.url) {
-                return false
-            }
-            return true
-        },
-    },
-    computed: {
-        isFormInvalid() {
-            return !this.pageTitle || !this.content || !this.linkText || !this.linkUrl
-        },
-    },
-    data() {
-        return {
-            pageTitle: '',
-            content: '',
-            linkText: '',
-            linkUrl: '',
-            published: true,
-        }
-    },
-    methods: {
-        submitForm() {
-            if (!this.pageTitle || !this.content || !this.linkText || !this.linkUrl) {
-                alert('Please fill in the form')
-                return
-            }
+<script setup>
+import {ref, inject, computed, watch} from 'vue'
+import {useRouter} from 'vue-router'
 
-            this.$emit('pageCreated', {
-                pageTitle: this.pageTitle,
-                content: this.content,
-                link: {
-                    text: this.linkText,
-                    url: this.linkUrl
-                },
-                published: this.published
-            })
-            this.clearForm()
-        },
-        clearForm() {
-            this.pageTitle = ''
-            this.content = ''
-            this.linkText = ''
-            this.linkUrl = ''
-            this.published = true
-        },
-    },
-    watch: {
-        // to watch changes on state (new and old value)
-        pageTitle(newTitle, oldTitle) {
-            if (this.linkText === oldTitle) {
-                this.linkText = newTitle
-            }
-        },
+const bus = inject('$bus')
+const pages = inject('$pages')
+const router = useRouter()
+
+let pageTitle = ref('')
+let content = ref('')
+let linkText = ref('')
+let published = ref(true)
+
+function submitForm() {
+    if (!pageTitle || !content || !linkText) {
+        alert('Please fill in the form')
+        return
     }
-}
-</script>
 
+    let newPage = {
+        pageTitle,
+        content,
+        link: {
+            text: linkText
+        },
+        published
+    }
+
+    pages.addPage(newPage)
+    bus.$emit('page-created', newPage)
+    router.push({path: '/pages'})
+}
+
+// computed
+const isFormInvalid = computed(() => !pageTitle || !content || !link.text)
+
+// watcher
+watch(pageTitle, (newTitle, oldTitle) => {
+    if (linkText === oldTitle) {
+        linkText = newTitle
+    }
+})
+</script>
 <style>
 
 </style>
